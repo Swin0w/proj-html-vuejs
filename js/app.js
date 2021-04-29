d6559e7a92744982f9dedb09fd1fb629bfc9bb22
Simone <marzolla.istruzione@gmail.com>
2021-04-30 00:55:07 +0200

    Carico

--- js/app.js
+++ js/app.js
@@ -2,10 +2,28 @@ function init() {
   new Vue({
     el: '#app',
     data: {
-
+      isActive: false,
+      index: 0
     },
-    methods:
-      
+    methods: {
+      // Slide precedente
+      prev: function() {
+        console.log("hello");
+      },
+      // Slide successivo
+      next: function() {
+        this.isActive == false;
+      }
+    },
+    // Slider automatico
+    mounted: function() {
+      setInterval(() => {
+        this.next();
+      }, 3000);
+    }
   });
+}
+function init() {
+  initVue();
 }
 document.addEventListener('DOMContentLoader', init);
